c32885197d30c4b697be0b76b84509f15a8f615f
Vincenzo Palazzo <vincenzopalazzodev@gmail.com>
2022-09-19 10:56:18 +0000

    docker: reintroduce poetry and improve the install
    Signed-off-by: Vincenzo Palazzo <vincenzopalazzodev@gmail.com>

--- contrib/docker/scripts/build.sh
+++ contrib/docker/scripts/build.sh
@@ -23,8 +23,8 @@ export LIGHTNINGD_POSTGRES_NO_VACUUM=1
 
 pip3 install --upgrade pip
 pip3 install --user poetry
-poetry export --dev --without-hashes -o requirements.txt
-pip3 install -r requirements.txt
+poetry config virtualenvs.create false --local
+poetry install --no-interaction --no-ansi --no-dev --no-root
 
 git clone https://github.com/lightningnetwork/lightning-rfc.git ../lightning-rfc
 git submodule update --init --recursive
